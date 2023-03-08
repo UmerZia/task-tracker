3e96584573c3750b4d50fd62cc446b6170a40ff4
UmerZia <omerzia73@gmail.com>
2023-03-08 20:25:06 +0500

    Install react-icons and Add delete item functionality

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,14 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import Task from "./Task";
 
-const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food shopping',
-        day: 'Feb 5th at 2:30pm',
-        remainder: false,
-    }
-])
+const Tasks = ({ tasks, onDelete }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onDelete={onDelete} />
+      ))}
+    </>
+  );
+};
 
-const Tasks = () => {
-    return (
-        <>
-            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
-        </>
-    )
-}
-
-export default Tasks
+export default Tasks;
