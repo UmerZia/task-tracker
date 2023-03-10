86aa1477a1521b090cb810fd6f9ff2db2bbbf081
UmerZia <omerzia73@gmail.com>
2023-03-10 19:22:49 +0500

    Toggle submit setion visibility

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import Button from './Button'
+import React from "react";
+import Button from "./Button";
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log('click')
-    }
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
-    <header className='header'>
-        <h1>{title}</h1>
-        <Button color='green' text='Add' onClick={onClick}/>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
